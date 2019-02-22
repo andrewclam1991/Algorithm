88ecffa31b9147741aed2f79c769ca280d99eb3c
Andrew Lam <23492362+andrewclam1991@users.noreply.github.com>
2019-02-21 23:34:42 -0800

    adds grows and shrinks test

--- ch1fundamentals/src/test/java/com/andrewclam/ch1fundamentals/section03/stack/ResizableStackTest.java
+++ ch1fundamentals/src/test/java/com/andrewclam/ch1fundamentals/section03/stack/ResizableStackTest.java
@@ -21,21 +21,42 @@ public class ResizableStackTest {
   }
 
   @Test
-  public void whenStackExceedsCapacity_stackGrows(){
+  public void whenMoreItemsThanCapacityArePushed_stackGrows(){
     // When stack of some object is created
     ResizableStack<String> stack = new ResizableStack<>(3);
-    // Assert stack size is 5
+    // Assert stack size is at first at 3
     assert stack.size() == 3;
     stack.push("a");
     stack.push("b");
     stack.push("c");
     stack.push("overflow?");
-    // Assert stack size is not 3
+    // When pushes another item that would exceed the capacity 3
+    // Assert stack size is no longer 3, and that it is in fact larger
     System.out.println(stack.size());
-    assert stack.size() != 3;
+    assert stack.size() != 3 && stack.size() > 3;
     stack.push("something");
     stack.push("something");
     stack.push("something");
     System.out.println(stack.size());
+  }
+
+  @Test
+  public void whenMoreThanHalfOfTheItemsArePopped_stackShrinks(){
+    // When stack of some object is created
+    ResizableStack<Integer> stack = new ResizableStack<>(10);
+    // And when it is initialized with 10 items
+    for(int i = 0; i < 10; i++){
+      stack.push(i);
+    }
+    // Assert the stack size is at first 10
+    assert stack.size() == 10;
+
+    // When stack is popped 5 times
+    for(int i = 0; i < 5; i++){
+      stack.pop();
+    }
+
+    // Assert that the stack is now half its original size
+    assert stack.size() == 5;
   }
 }
